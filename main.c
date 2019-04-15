e05d4d961f25e48cd083dac538ea2198fe153d73
Qiana Zhang <qiana_zhang@hotmail.com>
2019-04-15 12:33:10 +0000

    RoDecrypt does nothing

--- main.c
+++ main.c
@@ -2,35 +2,49 @@
 //encryption given k = 1
 
 char upperCase (char *x, int y); //make all letters in an array upper case
-//char rotate (char *x, int y ) //x is the message, y is the size of the message array,
+char roEncrypt(char *x, int y, int k, int i); //does rotation encryption with given k where x is the message, y is the size of the message array,
+//i is the array number
+char roDecrypt(char *x, int y, int i, int k);
 
 int main(){
-    int k = 0, i = 0; //i is the index of the array to be used later
+    int k = 0, i = 0;
     k = 1; //k is the rotation amount
     char message[] = {"Where did you hide the body?"};
-    
     int size = sizeof(message)/sizeof(char) - 1; // create interger 'size' and is the length of the array
     
     upperCase(message, size); //calling function to make all letters upper case
-    printf("%s\n", message); //printing the original message
+    printf("Original:   %s\n", message); //printing the original message
     
-    for (i = 0; i < sizeof(message)/sizeof(char) - 1; i++){ //rotating each character by k and prints each letter
-        if (message[i] + k > 90){ //if adding k gives a value greater than Z, then minus 26 to make a value between A and Z
-            message[i] = message[i] + k - 26;
-            printf("%c", message[i]);
-        }
-        else if (message[i] + k <= 90 && message[i] >64){//if adding k gives value between A and Z, then just leave it at adding k
-            message[i] += k;
-            printf("%c", message[i]);
-        }
-        else
-            printf("%c", message[i]); //if it's some other ASCII punctuation, just pring it as is
+    //Rotation Encryption
+    printf("Encrypted:  ");
+    for (i = 0; i < size; i++){
+    printf("%c", roEncrypt(message, size, k, i));
     }
+    //
+    //
+    //
+    //
+    //
+    //
+    //Rotation Decryption Not Given Key
+    printf("\nDecrypted:  ");
+    for (i =0, k = 1; i < size, k < 27; i++){
+    printf("%c", roDecrypt(message, size, i, k));
+    }
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    
 return 0;
 }
 
 
-
+//FUNCTION DEFINITIONS
 char upperCase (char *x, int y) { //make all letters in an array upper case
     int i;
     for (i = 0; i < y; i++){ //make everything upper case
@@ -40,3 +54,29 @@ char upperCase (char *x, int y) { //make all letters in an array upper case
         }
         return 0;
 }
+
+char roEncrypt(char *x, int y, int k, int i) {
+    if (x[i] + k > 90){ //if adding k gives a value greater than Z, then minus 26 to make a value between A and Z
+        x[i] = x[i] + k - 26;
+        return x[i];
+        }
+    else if (x[i] + k <= 90 && x[i] >64){//if adding k gives value between A and Z, then just leave it at adding k
+        x[i] += k;
+        return x[i];
+        }
+    else
+        return x[i]; //if it's some other ASCII punctuation, just print it as is
+    }
+    
+char roDecrypt(char *x, int y, int i, int k){
+    if{   
+       x[i] = x[i] - k - 26;
+        return x[i];
+        }
+    else if (x[i] + k <= 90 && x[i] >64){//if adding k gives value between A and Z, then just leave it at adding k
+        x[i] -= k;
+        return x[i];
+        }
+    else
+        return x[i]; //if it's some other ASCII punctuation, just print it as is
+    }   
